ef75c6c1e877a06f6e1c518f771234fd29b23c56
Allan <altonallan47@gmail.com>
2024-08-23 19:44:26 +0200

    Update RouteJS

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,30 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from "@utils/database";
+
 
 const handler = NextAuth ({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
-
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({session}) {
 
     },
     async signIn({profile}) {
+        try {
+            await connectToDB();
 
+            //Check if user already Exists
+
+            //If NOT, create new user
+
+            return true;
+        } catch(error) {
+            console.log(error)
+        }
     }
 })
 
