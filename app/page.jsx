28cf8d1c5904a924420adaf760ccd65c15424396
Allan <altonallan47@gmail.com>
2024-08-23 16:21:53 +0200

    Create page.jsx
    Basic Homepage layout and Styling

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+        </h1>
+        <p>Promptly is a modern AI prompting tool for discovering and sharing 
+            creative prompts.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default Home
