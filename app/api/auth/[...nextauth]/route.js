b68427a0128aed2dfbacd3db41277be181a3da8b
Allan <altonallan47@gmail.com>
2024-08-24 17:21:43 +0200

    Import User to Routes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 
 const handler = NextAuth ({
