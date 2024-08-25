fef3ba2f0f5ce6f453d0a2b13cdc19d563d755bf
Allan <altonallan47@gmail.com>
2024-08-25 16:41:26 +0200

    Update next.config.mjs
    With ECMAScript

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'], // Add the domain here
+    },
+  };
+  
+  export default nextConfig;
+  
