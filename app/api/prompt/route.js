7f790aec81161bcd4a7712d9cae5903e57fa1e7f
Allan <altonallan47@gmail.com>
2024-08-27 16:02:18 +0200

    API Pathway

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,3 +1,12 @@
 // Ya Feed
 
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request) => {
+    try {
+        await connectToDB()''
+    } catch (error) {
+        
+    }
+}
