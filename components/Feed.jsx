55c31da0776f398b5c16acd148650b9f387c3df6
Allan <altonallan47@gmail.com>
2024-08-27 15:56:45 +0200

    Error Handling

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,7 +24,7 @@ const Feed = () => {
 
       setPosts(data);
     }
-    
+
     fetchPosts();
   }, []);
 
@@ -42,7 +42,7 @@ const Feed = () => {
       </form>
 
       <PromptCardList 
-          data={[]}
+          data={[posts]}
           handleTagClick={() => {}}/>
     </section>
   )
