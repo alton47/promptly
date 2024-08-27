bdc55ae707de69f3140f8e5a74a0be08c28ce00c
Allan <altonallan47@gmail.com>
2024-08-27 15:42:22 +0200

    Update Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
 
 const Feed = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+    className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+           type='text' />
+      </form>
+    </section>
   )
 }
 
