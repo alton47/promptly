fe5cdc9aefc52025b7ce6c4bdb75d2ba32e6ac9a
Allan <altonallan47@gmail.com>
2024-08-27 16:14:46 +0200

    Error handling

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,20 @@ import { usePathname, useRouter } from "next/navigation";
 const PromptCard = ({ post, handleTagCLick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
-      Card
+      <div className="flex justify-between items-start gap-5">
+        <Image
+          scr={post.creator.image}
+          alt="user_image"
+          width={40}
+          height={40}
+          className="rounded-full object-contain"
+        />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900" 
+                {post.creator.username}></h3>
+          </div>
+      </div>
     </div>
   )
 }
