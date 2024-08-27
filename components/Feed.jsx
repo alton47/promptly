9c588651d194c116141de2be5440cfbf1655eeae
Allan <altonallan47@gmail.com>
2024-08-27 15:48:09 +0200

    Update Feed
    Add searches

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,12 +5,23 @@ import PromptCard from "./PromptCard";
 
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+
+  }
+
+  
   return (
     <section
     className="feed">
       <form className="relative w-full flex-center">
         <input 
-           type='text' />
+           type='text'
+           placeholder='Search for a tag or Username'
+           value={searchText}
+           onChange={handleSearchChange}
+           required
+           className="search_input peer" />
       </form>
     </section>
   )
