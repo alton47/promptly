f6347104b643d0a93e912d596c9faeb801c6fa13
Allan <altonallan47@gmail.com>
2024-08-27 15:54:56 +0200

    Update Feed,jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,12 +3,26 @@
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout"></div>
+  )
+}
+
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
   const handleSearchChange = (e) => {
 
   }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json
+    }
+  }, []);
 
   return (
     <section
