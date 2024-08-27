fda1ab3680d8094b0fa8b8c78349f8592fa79b7c
Allan <altonallan47@gmail.com>
2024-08-27 16:09:10 +0200

    Update PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+'use client'
 
-const PromptCard = () => {
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+
+const PromptCard = ({ post, handleTagCLick, handleEdit, handleDelete }) => {
   return (
-    <div>
+    <div className="prompt_card">
       Card
     </div>
   )
